1c18d908c6fec2a507c0703ddf7ed2f139fc0408
RekhisAmir <amir.rekhis@gmail.com>
2021-03-27 12:28:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Test from "./Test"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+function App() {
+  return (
+  <div> 
+     <Test />
+  </div>
+  );
+}
+
+export default App;
